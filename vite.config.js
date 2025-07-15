42def5c8ee6b1b99ba1757b3ea081bac4ab584a7
singhr45625 <kpuja0969@gmail.com>
2025-07-16 00:06:22 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  base: "/chat_app/", // Keep this if deploying to /chat_app subpath
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy-1",// Keep this if deploying to /chat_app subpath
   plugins: [
     react(),
     visualizer({
